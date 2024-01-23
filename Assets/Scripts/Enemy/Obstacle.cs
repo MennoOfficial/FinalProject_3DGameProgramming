01eedc545f1e7cdc32089d2666ef28b44ba0947c
Menno Plochaet <145544689+MennoOfficial@users.noreply.github.com>
2024-01-23 14:49:51 +0100

    fix fire bug

--- Assets/Scripts/Enemy/Obstacle.cs
+++ Assets/Scripts/Enemy/Obstacle.cs
@@ -5,49 +5,46 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
     public int damage = 0;
-
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
-
-            //Debug.Log("Contact");
-
             if (playerHealth != null)
             {
                 if (playerHealth.health - damage > 0)
                 {
                     playerHealth.Damage(damage);
                 }
-                else 
+                else
                 {
                     playerHealth.Damage(playerHealth.health);
                     KillPlayer();
                 }
+
             }
 
         }
 
         // Testing damage + HP
-/*        if (other.CompareTag("Target"))
-        {
-            Target target = other.GetComponent<Target>();
+        /*        if (other.CompareTag("Target"))
+                {
+                    Target target = other.GetComponent<Target>();
 
-            if (target != null)
-            {
-                if (target.health > 0)
-                {
-                    target.Damage(damage);
-                }
-                else
-                {
-                    Debug.Log("Target has been killed");
-                }
-            }
-        }*/
+                    if (target != null)
+                    {
+                        if (target.health > 0)
+                        {
+                            target.Damage(damage);
+                        }
+                        else
+                        {
+                            Debug.Log("Target has been killed");
+                        }
+                    }
+                }*/
     }
-
     private void KillPlayer()
     {
         // You can implement the logic for player death here.
