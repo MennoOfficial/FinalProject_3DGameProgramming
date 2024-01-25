7834402bae83fce24ea6e70c5b109664af7cb2d0
MennoOfficial <plochaetm@gmail.com>
2024-01-25 01:27:52 +0100

    bug fix

--- Assets/Scripts/Finish/Finish.cs
+++ Assets/Scripts/Finish/Finish.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static UnityEngine.GraphicsBuffer;
 
 public class Finish : MonoBehaviour
 {
     // Start is called before the first frame update
 
     private bool InRange = false;
+    Target target;
+    public GameObject targets;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -19,7 +22,7 @@ public class Finish : MonoBehaviour
 
     void Start()
     {
-        
+        target = targets.GetComponent<Target>();
     }
 
     // Update is called once per frame
@@ -27,6 +30,7 @@ public class Finish : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E) && InRange)
         {
+            target.Score = 0;
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
